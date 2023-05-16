26b20822ae4152b04000d659f2684d4524c51240
SoldadoZuero <skeleton_fake@outlook.com>
2023-05-16 12:27:20 -0300

    Learned about context into a render

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html")
+    return render(request, "recipes/home.html", context={"name": "Soldado"})
 
 
 def contact(request):
