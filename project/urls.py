310af8f956587a6952e9a4571faddfbf985fe5f9
SoldadoZuero <skeleton_fake@outlook.com>
2023-05-22 16:22:44 -0300

    Started to match models with recipe.html

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 # HTTP REQUEST
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("recipes.urls"))
     ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
