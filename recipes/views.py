10bca67349cb20c95eabf8a0409630e2b4586937
SoldadoZuero <skeleton_fake@outlook.com>
2023-05-23 14:13:29 -0300

    Some new improvements

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -15,14 +15,20 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True).order_by('-id')
+
+    # if not recipes:
+    #     raise Http404('Not found ツ')
+    recipes = get_list_or_404(Recipe)
+
     return render(
         request,
         "recipes/pages/category.html",
         context={
             'recipes': recipes,
+            'title': f'{recipes.first().category.name} - Category | '
         }
     )
 
