f229fd4c64366570d3ce6dca5a846870dffbbb6e
SoldadoZuero <skeleton_fake@outlook.com>
2023-05-24 16:46:46 -0300

    changed recipe destiny

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_list_or_404
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 
@@ -15,13 +14,10 @@ def home(request):
 
 
 def category(request, category_id):
-    # recipes = Recipe.objects.filter(
-    #     category__id=category_id,
-    #     is_published=True).order_by('-id')
 
-    # if not recipes:
-    #     raise Http404('Not found ツ')
-    recipes = get_list_or_404(Recipe)
+    recipes = get_list_or_404(Recipe.objects.filter(
+                              category__id=category_id,
+                              is_published=True).order_by('-id'))
 
     return render(
         request,
@@ -34,11 +30,15 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True).order_by('-id').first()
+
     return render(
         request,
         "recipes/pages/recipe-view.html",
         context={
-            'recipe': make_recipe(),
+            'recipe': recipe,
             'is_detail_page': True,
         }
     )
