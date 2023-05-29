236c1af2c33ee8c28f87bb570265110620569076
SoldadoZuero <skeleton_fake@outlook.com>
2023-05-29 16:02:13 -0300

    Isolated test

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse, resolve # noqa: F401, E261,
+from django.urls import reverse
 
 
 class RecipeURLsTest(TestCase):
@@ -14,8 +14,3 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
-
-
-class RecipeViewsTest(TestCase):
-    def test_recipe_home_views_function_is_correct(self):
-        ...
