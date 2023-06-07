06cf844c17b63a5489d622850fe05f92a653d346
SoldadoZuero <skeleton_fake@outlook.com>
2023-06-07 15:55:00 -0300

    Finished request.GET module

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
+from django.db.models import Q
+from django.http.response import Http404
 
 
 def home(request):
@@ -45,3 +47,24 @@ def recipe(request, id):
             'is_detail_page': True,
         }
     )
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True,
+    ).order_by('-id')
+
+    return render(request, "recipes/pages/search.html", {
+        'page_title': f'Search for "{search_term}"',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
