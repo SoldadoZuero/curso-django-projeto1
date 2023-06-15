81c6182fafe3dcf475083c556a68ad0a6a8cf32f
SoldadoZuero <skeleton_fake@outlook.com>
2023-06-15 17:03:46 -0300

    Making users forms

--- authors/forms.py
+++ authors/forms.py
@@ -11,3 +11,26 @@ class RegisterForm(forms.ModelForm):
                   'email',
                   'password',
                   ]
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'Email',
+            'password': "Password',"
+        }
+        help_texts = {
+            'email': 'The email must be valid.',
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
