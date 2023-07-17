5737d2a6a5e1b41eeca9b19a9091b10cae88ee5a
SoldadoZuero <skeleton_fake@outlook.com>
2023-07-17 15:31:38 -0300

    moved settings.py to a package

---
+++ project/settings/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .environment import *  # isort:skip
+from .installed_apps import *  # isort:skip
+from .middlewares import *  # isort:skip
+from .templates import *  # isort:skip
+from .databases import *  # isort:skip
+from .security import *  # isort:skip
+from .i18n import *  # isort:skip
+from .assets import *  # isort:skip
+from .messages import *  # isort:skip
+from .debug_toolbar import *  # isort:skip
