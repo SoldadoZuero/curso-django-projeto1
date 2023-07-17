9c29b0500181695ca4381a7a6d7c609008b1b3ac
SoldadoZuero <skeleton_fake@outlook.com>
2023-07-17 14:45:37 -0300

    fixed allowed hosts

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
 from .models import Category, Recipe
-from tag.models import Tag
-from django.contrib.contenttypes.admin import GenericStackedInline
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
-
-
-class TagInline(GenericStackedInline):
-    model = Tag
-    fields = 'name',
-    extra = 1
 
 
 @admin.register(Recipe)
@@ -30,10 +22,7 @@ class RecipeAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',)
     }
-
-    inlines = [
-        TagInline
-    ]
+    autocomplete_fields = 'tags',
 
 
 admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.contrib.contenttypes.fields import GenericRelation
 from tag.models import Tag
 
 
@@ -36,7 +35,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
-    tags = GenericRelation(Tag, related_query_name='recipes')
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
