3957b7e01392ee7058466597d6b0a86c14d993ee
SoldadoZuero <skeleton_fake@outlook.com>
2023-08-01 08:00:17 -0300

    End of the pillow module

--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 
 from django.contrib.auth.models import User
 from django.db import models
+import os
+from django.conf import settings
 from django.db.models import F, Value
 from django.db.models.functions import Concat
 from django.forms import ValidationError
@@ -9,6 +11,7 @@ from django.urls import reverse
 from django.utils.text import slugify
 from tag.models import Tag
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
 
 class Category(models.Model):
@@ -64,12 +67,40 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
 
+    @staticmethod
+    def resize_image(image, new_width=1280):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_heigth = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_heigth = round((new_width * original_heigth) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_heigth), Image.LANCZOS)
+
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 1280)
+            except FileNotFoundError:
+                ...
+
+        return saved
 
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
